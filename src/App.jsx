1b229da8c69df95b4efa2a30f187206cab542e29
Sol Bae <b.seoyoung@wustl.edu>
2025-04-01 17:30:05 -0500

    created separate Square component and renamed original function into Board

--- src/App.jsx
+++ src/App.jsx
@@ -8,29 +8,35 @@ export default function App() {
 
   return (
     <>
-      <Square />
+      <Board />
     </>
   )
 }
 
-export function Square() {
+function Square() {
+  return <button className="square">1</button>
+}
+
+export function Board() {
   return (
     <>
       <div className="board-row">
-        <button className="square">1</button>
-        <button className="square">2</button>
-        <button className="square">3</button>
+        <Square />
+        <Square />
+        <Square />
       </div>
       <div className="board-row">
-        <button className="square">4</button>
-        <button className="square">5</button>
-        <button className="square">6</button>
+        <Square />
+        <Square />
+        <Square />
       </div>
       <div className="board-row">
-        <button className="square">7</button>
-        <button className="square">8</button>
-        <button className="square">9</button>
+        <Square />
+        <Square />
+        <Square />
       </div>
     </>
-  );
+  )
+  
 }
+
