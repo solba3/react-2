9b8554790851846fa5cb68e0e922cde481b9d109
Sol Bae <b.seoyoung@wustl.edu>
2025-04-01 18:05:48 -0500

    changed onclick handler so it calls set function

--- src/App.jsx
+++ src/App.jsx
@@ -13,9 +13,11 @@ export default function App() {
   )
 }
 
-function Square({ value }) {
+function Square() {
+  const [value, setValue] = useState(null);
+
   function handleClick() {
-    console.log('clicked!');
+    setValue('X');
   }
 
   return (
@@ -25,26 +27,26 @@ function Square({ value }) {
     >
       {value}
     </button>
-  )
+  );
 }
 
 export function Board() {
   return (
     <>
       <div className="board-row">
-        <Square value="1"/>
-        <Square value="2"/>
-        <Square value="3"/>
+        <Square />
+        <Square />
+        <Square />
       </div>
       <div className="board-row">
-        <Square value="4"/>
-        <Square value="5"/>
-        <Square value="6"/>
+        <Square />
+        <Square />
+        <Square />
       </div>
       <div className="board-row">
-        <Square value="7"/>
-        <Square value="8"/>
-        <Square value="9"/>
+        <Square />
+        <Square />
+        <Square />
       </div>
     </>
   )
